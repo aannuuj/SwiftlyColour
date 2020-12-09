43cd830277d109113599f0f335a2ef611345da87
aannuuj <hariompalkar@gmail.com>
2020-12-09 17:46:47 +0530

    Initial Commit

---
+++ SwiftlyColour/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftlyColour
+//
+//  Created by Hariom Palkar on 09/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
